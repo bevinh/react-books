00f108f295c40211499199381df87e679cf223ab
bevinh <bevin.hernandez@gmail.com>
2017-07-26 22:59:07 -0400

    moved SearchPage to it's own file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks';
-import Book from './Book';
-
+import SearchPage from './SearchPage';
 class App extends Component {
     state = {
         books: [],
@@ -18,32 +17,7 @@ class App extends Component {
       <div className="App">
           <div className="app">
               {this.state.showSearchPage ? (
-                      <div className="search-books">
-                          <div className="search-books-bar">
-                              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-                              <div className="search-books-input-wrapper">
-                                  {/*
-                                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                                   You can find these search terms here:
-                                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                                   you don't find a specific author or title. Every search is limited by search terms.
-                                   */}
-                                  <input type="text" placeholder="Search by title or author"/>
-
-                              </div>
-                          </div>
-                          <div className="search-books-results">
-                              <ol className="books-grid">
-                                  {this.state.books.map((book) => (
-                                      <li key={book.id}>
-                                          <Book title={book.title} cover={book.imageLinks.thumbnail} authors={book.authors} />
-                                      </li>
-                                  ))}
-                              </ol>
-                          </div>
-                      </div>
+                    <SearchPage books={this.state.books} />
                   ) : (
                       <ListBooks books={this.state.books}/>
 
